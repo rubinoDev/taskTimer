4a2a4bfc4ea5e6d0c0344c18f7621d2fd5a6b2a2
Victor Rubino <rubinodev@gmail.com>
2022-08-26 16:22:15 -0300

    router outlet and default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
